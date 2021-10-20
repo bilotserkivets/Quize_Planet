e697690df4ee2895749eb036eb700a3329b64c98
bilotserkivets <33667831+bilotserkivets@users.noreply.github.com>
2021-10-20 21:18:40 +0300

    Bug fixes

--- app/src/main/java/com/example/quizontheplanet/QuestionFragment.kt
+++ app/src/main/java/com/example/quizontheplanet/QuestionFragment.kt
@@ -5,15 +5,78 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.quizontheplanet.databinding.FragmentQuestionBinding
 
 class QuestionFragment : Fragment() {
+    lateinit var binding: FragmentQuestionBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentQuestionBinding.inflate(inflater)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_question, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val questionId = arguments?.getInt("questionId")
+        when (questionId) {
+            R.id.tvQuestion1 -> {
+                binding.headerQuestion.text = getString(R.string.question_one)
+            }
+            R.id.tvQuestion2 -> {
+                binding.headerQuestion.text = getString(R.string.question_two)
+            }
+            R.id.tvQuestion3 -> {
+                binding.headerQuestion.text = getString(R.string.question_three)
+            }
+        }
+
+        val planets = listOf<View>(
+            binding.tvMercury,
+            binding.tvVenus,
+            binding.tvEarth,
+            binding.tvMars,
+            binding.tvJupiter,
+            binding.tvSaturn,
+            binding.tvUranus,
+            binding.tvNeptune
+        )
+        for (v in planets) {
+            v.setOnClickListener {
+                var message = ""
+                when (questionId) {
+                    R.id.tvQuestion1 -> {
+                        if (v.id == R.id.tvJupiter) {
+                            message = getString(R.string.jupiter_answer, getString(R.string.correct))
+                        } else {
+                            message = getString(R.string.jupiter_answer, getString(R.string.wrong))
+                        }
+                    }
+                    R.id.tvQuestion2 -> {
+                        if (v.id == R.id.tvSaturn) {
+                            message = getString(R.string.saturn_answer, getString(R.string.correct))
+                        } else {
+                            message = getString(R.string.saturn_answer, getString(R.string.wrong))
+                        }
+                    }
+                    R.id.tvQuestion1 -> {
+                        if (v.id == R.id.tvUranus) {
+                            message = getString(R.string.uranus_answer, getString(R.string.correct))
+                        } else {
+                            message = getString(R.string.uranus_answer, getString(R.string.wrong))
+                        }
+                    }
+                }
+                val bundle = Bundle()
+
+                bundle.putString("message", message)
+                (activity as MainActivity).navController.navigate(R.id.action_questionFragment_to_answerFragment, bundle)
+            }
+        }
     }
 
     companion object {
--- app/src/main/java/com/example/quizontheplanet/StartFragment.kt
+++ app/src/main/java/com/example/quizontheplanet/StartFragment.kt
@@ -5,15 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.quizontheplanet.databinding.FragmentStartBinding
 
 class StartFragment : Fragment() {
+    lateinit var binding: FragmentStartBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentStartBinding.inflate(inflater)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val questions = listOf<View>(
+            binding.tvQuestion1,
+            binding.tvQuestion2,
+            binding.tvQuestion3
+        )
+
+        for (v in questions) {
+            v.setOnClickListener{
+                val bundle = Bundle()
+                val questionId = v.id
+                bundle.putInt("questionId", questionId )
+                (activity as MainActivity).navController.navigate(R.id.action_startFragment_to_questionFragment, bundle)
+            }
+        }
     }
 
     companion object {
